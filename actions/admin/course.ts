99986641900161f7e58a439c0c531278114d447f
Abhiram <vemulaabhiram2004@gmail.com>
2024-11-10 16:15:37 +0530

    get course modules

--- actions/admin/course.ts
+++ actions/admin/course.ts
@@ -1,5 +1,6 @@
 "use server";
 import { prisma } from "@/prisma";
+
 export const createCourse = async (name: string, image: string) => {
   try {
     await prisma.course.create({
@@ -11,5 +12,23 @@ export const createCourse = async (name: string, image: string) => {
     return { success: true, res: 200 };
   } catch (error) {
     console.log("Error in actions>admin>course.ts>createCourse", error);
+  }
+};
+
+export const getCourseModules = async (course_id: string) => {
+  try {
+    const course = await prisma.course.findFirst({
+      where: {
+        course_id,
+      },
+      include: {
+        modules: true,
+      },
+    });
+    if (!course) return [];
+    return course.modules;
+  } catch (error) {
+    console.log("Error in actions/admin/course.ts > getCourseModules", error);
+    return [];
   }
 };
